2770342e81228b88fab77d71a536533260fd4e7e
JD Macarubbo <jdmacarubbo@comscentre.com>
2019-04-10 15:23:54 +0800

    Added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   module: {
     rules: [
       {
