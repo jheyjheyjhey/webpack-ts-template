5a986bf6aac5e7b659131760f36cced4201edfbf
JD Macarubbo <jdmacarubbo@comscentre.com>
2019-04-10 14:56:54 +0800

    Added config for styles

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as _ from 'lodash';
+import './css/style.css';
 
 function component() {
   let element = document.createElement('div')
   element.innerHTML = _.join(['test', 1], " ")
+  element.classList.add('red');
   return element
 }
 
