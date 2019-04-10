41c240805efa719a2458298f2d386e7597bcc8c7
JD Macarubbo <jdmacarubbo@comscentre.com>
2019-04-10 15:16:29 +0800

    Added clean-up routine to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.ts',
@@ -28,6 +29,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['!./dist/.gitignore']
+    }),
     new HtmlWebpackPlugin({
       minify: true,
       template: './src/index.html'
